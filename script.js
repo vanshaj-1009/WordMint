bf305fcb1a597a8a65061f1db39a70198349eca8
vanshaj-1009 <guptavanshaj2023@gmail.com>
2025-08-11 16:57:18 +0530

    Adding Login/out Functionality

---
+++ script.js
@@ -0,0 +1,11 @@
+/* Show the registration form modal */
+function showRegisterForm() {
+    document.getElementById('registerModal').classList.remove('hidden');
+    document.getElementById('regUsername').focus();
+}
+
+/* Show the login form modal */
+function showLoginForm() {
+    document.getElementById('loginModal').classList.remove('hidden');
+    document.getElementById('loginUsername').focus();
+}
